96e6741f67e6ceb11b3fa42ff1394a9a9e8959f8
polevshicov <polevshicov@users.noreply.github.com>
2026-08-24 18:37:56 +0500

    Fix: remove duplicate version catalog 'from' call

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,11 +18,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
+
 }
 
 rootProject.name = "CeilingCalculator"
